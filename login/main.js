4b4763007f293c3901d922f59728093f79fb3a67
gustianaditamap <gustianaditamap@gmail.com>
2020-05-12 22:08:04 +0700

    fix tampilan login

--- login/main.js
+++ login/main.js
@@ -1,3 +1,7 @@
 document.querySelector('.login').addEventListener('click',()=>{
     window.open('../homepage/homepage.html')
 })
+
+document.querySelector('.daftar').addEventListener('click',()=>{
+    window.open('../homepage/homepage.html')
+})
